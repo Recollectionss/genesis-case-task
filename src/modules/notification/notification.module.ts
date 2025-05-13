2e0041f8de82b144e782ab38e536b2ab0860b423
Observersss <heavyobjectics@gmail.com>
2025-05-14 02:01:56 +0300

    rename mail-transporter.ts to mail-transporter.providers.ts

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
-import { TRANSPORTER } from './mail-transporter';
 import { ConfigModule } from '@nestjs/config';
 import smtpConfig from '../../config/smtp.config';
+import { TRANSPORTER } from './mail-transporter.providers';
 
 @Module({
   imports: [ConfigModule.forRoot({ load: [smtpConfig] })],
