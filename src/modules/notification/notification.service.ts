512b515f498993fdf2b46e3b7d6aac01def79c3c
Observersss <heavyobjectics@gmail.com>
2025-05-14 02:04:47 +0300

    write into notification.service.ts sendMail method also add logger if new method throw error

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -1,14 +1,38 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { MAIL_TRANSPORTER } from './mail-transporter';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import smtpConfig from '../../config/smtp.config';
 import { ConfigType } from '@nestjs/config';
 import { Transporter } from 'nodemailer';
+import { ISendMail } from '../../constants/mail.types.constants';
+import { MAIL_DETAILS } from './constants/mail.details.constants';
+import { MAIL_TRANSPORTER } from './mail-transporter.providers';
 
 @Injectable()
 export class NotificationService {
+  private readonly logger = new Logger(NotificationService.name);
+
   constructor(
     @Inject(MAIL_TRANSPORTER) private readonly transporter: Transporter,
     @Inject(smtpConfig.KEY)
     private readonly smtpConf: ConfigType<typeof smtpConfig>,
   ) {}
+
+  async sendMail(data: ISendMail) {
+    try {
+      const mailData = {
+        from: this.smtpConf.user,
+        to: data.to,
+        subject: MAIL_DETAILS[data.type].subject + data.city,
+        text: MAIL_DETAILS[data.type].text + data.token,
+      };
+      await this.transporter.sendMail(mailData);
+    } catch (err) {
+      this.logger.error('Error', err);
+      throw new InternalServerErrorException(err.message);
+    }
+  }
 }
