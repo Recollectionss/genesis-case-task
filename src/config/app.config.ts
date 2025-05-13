7bdd39394ddd9b58647f6f78b9db81bc5799aad5
Observersss <heavyobjectics@gmail.com>
2025-05-13 21:07:54 +0300

    add env.constant.ts, use type ENV in app.config.ts for node_env

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,8 @@
 import { registerAs } from '@nestjs/config';
 import * as process from 'node:process';
+import { ENV } from '../constants/env.constant';
 
 export default registerAs('app', () => ({
   port: Number(process.env.APP_PORT),
-  node_dev: String(process.env.NODE_ENV),
+  node_dev: String(process.env.NODE_ENV) as ENV,
 }));
