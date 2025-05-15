8f2f58b2181c16f1bfae03501fe6318be7fae2d5
Observersss <heavyobjectics@gmail.com>
2025-05-15 20:24:11 +0300

    install class-transformer, and class-validator, add constants.ts to weather-api for create url builder

---
+++ src/modules/weather-api/constants/constants.ts
@@ -0,0 +1,13 @@
+export const WEATHER_API_BASE_URL = 'https://api.weatherapi.com/v1';
+
+export const WEATHER_API_PATHS = {
+  CURRENT: '/current',
+};
+
+export const WEATHER_API_TYPE_RESPONSE = {
+  JSON: '.json',
+};
+
+export const WEATHER_API_PARAMS = {
+  lang: 'en',
+};
