f2a33bde0c614a219f89de970adf70c6cbade754
Observersss <heavyobjectics@gmail.com>
2025-05-16 01:07:41 +0300

    add migration for city

---
+++ src/modules/postgres/migrations/20250515220405-add_city.js
@@ -0,0 +1,31 @@
+'use strict';
+const schema = process.env.NODE_ENV === 'production' ? 'public' : 'test';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      { tableName: 'city', schema },
+      {
+        id: {
+          type: Sequelize.UUID,
+          unique: true,
+          allowNull: false,
+          primaryKey: true,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+      },
+    );
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeIndex({ tableName: 'city', schema }, ['email']);
+    await queryInterface.removeIndex({ tableName: 'city', schema }, ['id']);
+    await queryInterface.dropTable({ tableName: 'city', schema });
+  },
+};
